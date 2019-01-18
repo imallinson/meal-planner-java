039f942536b40f4c9785042ebb841bd359e2fc3d
imallinson <imallinson1001@gmail.com>
2019-01-18 18:14:43 +0000

    entity relationships

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,12 +1,24 @@
 package com.qa.persistence.domain;
 
-import javax.persistence.*;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity; 
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Account {
 	@Id
 	private String username;
 	private String password;
+	@OneToMany(mappedBy="username", cascade=CascadeType.PERSIST)
+	private List<Meal> meals;
+	@OneToMany(mappedBy="username", cascade=CascadeType.PERSIST)
+	private List<Recipe> recipes;
+	@OneToOne(mappedBy="username", cascade=CascadeType.PERSIST)
+	private ShoppingList shoppingList;
 	
 	public String getPassword() {
 		return password;
@@ -19,5 +31,17 @@ public class Account {
 	public String getUsername() {
 		return username;
 	}
-		
+
+	public List<Meal> getMeals() {
+		return meals;
+	}
+
+	public List<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public ShoppingList getShoppingList() {
+		return shoppingList;
+	}
+			
 }
--- src/main/java/com/qa/persistence/domain/ShoppingList.java
+++ src/main/java/com/qa/persistence/domain/ShoppingList.java
@@ -1,8 +1,19 @@
 package com.qa.persistence.domain;
 
+import java.util.List;
+
 import javax.persistence.*;
 
 @Entity
 public class ShoppingList {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long shoppingListID;
+	@OneToMany(mappedBy="shoppingListID", cascade=CascadeType.PERSIST)
+	private List<Ingredient> ingredients;
 
+	public Long getShoppingListID() {
+		return shoppingListID;
+	}
+	
 }
