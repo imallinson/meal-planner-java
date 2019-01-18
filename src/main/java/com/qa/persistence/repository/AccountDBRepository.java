771adc20d0be82671cbbc8fda938910fe046ad06
imallinson <imallinson1001@gmail.com>
2019-01-18 21:21:30 +0000

    removed unused import

--- src/main/java/com/qa/persistence/repository/AccountDBRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountDBRepository.java
@@ -2,21 +2,16 @@ package com.qa.persistence.repository;
 
 import static javax.transaction.Transactional.TxType.*;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import com.qa.persistence.domain.Account;
-import com.qa.util.JSONUtil;
 
 @Transactional(SUPPORTS)
 public class AccountDBRepository implements AccountRepository {
 	@PersistenceContext(unitName = "primary")
 	private EntityManager manager;
-
-	@Inject
-	private JSONUtil util;
 
 	@Override
 	public boolean checkAccount(Account account) {
