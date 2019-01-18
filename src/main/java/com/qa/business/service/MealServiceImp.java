56bbe1ee759b18f11bb9fa758dcfb27c151df213
imallinson <imallinson1001@gmail.com>
2019-01-18 20:39:11 +0000

    meal service

--- src/main/java/com/qa/business/service/MealServiceImp.java
+++ src/main/java/com/qa/business/service/MealServiceImp.java
@@ -2,34 +2,39 @@ package com.qa.business.service;
 
 import javax.inject.Inject;
 
+import com.qa.persistence.domain.Meal;
+import com.qa.persistence.repository.MealRepostiory;
 import com.qa.util.JSONUtil;
 
 public class MealServiceImp implements MealService {
+	@Inject
+	MealRepostiory repo;
+	
 	@Inject
 	private JSONUtil util;
 	
 	@Override
 	public String getMeal(String mealJSON) {
-		// TODO Auto-generated method stub
-		return null;
+		Meal meal = util.getObjectForJSON(mealJSON, Meal.class);
+		return repo.getMeal(meal);
 	}
 
 	@Override
 	public String createMeal(String mealJSON) {
-		// TODO Auto-generated method stub
-		return null;
+		Meal meal = util.getObjectForJSON(mealJSON, Meal.class);
+		return repo.createMeal(meal);
 	}
 
 	@Override
 	public String deleteMeal(String mealJSON) {
-		// TODO Auto-generated method stub
-		return null;
+		Meal meal = util.getObjectForJSON(mealJSON, Meal.class);
+		return repo.deleteMeal(meal);
 	}
 
 	@Override
 	public String updateMeal(String mealJSON) {
-		// TODO Auto-generated method stub
-		return null;
+		Meal meal = util.getObjectForJSON(mealJSON, Meal.class);
+		return repo.updateMeal(meal);
 	}
 
 }
