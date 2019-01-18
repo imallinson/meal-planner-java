928d071eaae1bef7a6800bc7f208dd2ac3fe50f0
imallinson <imallinson1001@gmail.com>
2019-01-18 20:06:33 +0000

    meal endpoints

--- src/main/java/com/qa/business/service/MealService.java
+++ src/main/java/com/qa/business/service/MealService.java
@@ -2,4 +2,12 @@ package com.qa.business.service;
 
 public interface MealService {
 
+	String getMeal(String mealJSON);
+
+	String createMeal(String mealJSON);
+
+	String deleteMeal(String mealJSON);
+
+	String updateMeal(String mealJSON);
+
 }
--- src/main/java/com/qa/rest/MealEndpoint.java
+++ src/main/java/com/qa/rest/MealEndpoint.java
@@ -14,7 +14,29 @@ public class MealEndpoint {
 	@Inject
 	private MealService service;
 
+	@Path("/get")
+	@GET
+	public String getMeal(String mealJSON) {
+		return service.getMeal(mealJSON);
+	}
 	
+	@Path("/create")
+	@POST
+	public String createMeal(String mealJSON) {
+		return service.createMeal(mealJSON);
+	}
+	
+	@Path("/delete")
+	@DELETE
+	public String deleteMeal(String mealJSON) {
+		return service.deleteMeal(mealJSON);
+	}
+	
+	@Path("/update")
+	@PUT
+	public String updateMeal(String mealJSON) {
+		return service.updateMeal(mealJSON);
+	}
 
 	public void setService(MealService service) {
 		this.service = service;
