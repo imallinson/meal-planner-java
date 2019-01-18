730ce1202a5409efd8a8b215f9a58c6a9dbc7618
imallinson <imallinson1001@gmail.com>
2019-01-18 08:50:43 +0000

    meal and recipe endpoints

--- src/main/java/com/qa/rest/MealEndpoint.java
+++ src/main/java/com/qa/rest/MealEndpoint.java
@@ -1,5 +1,44 @@
 package com.qa.rest;
 
-public class MealEndpoint {
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 
+import com.qa.business.service.MealService;
+
+@Path("/meal")
+public class MealEndpoint {
+	@Inject
+	private MealService service;
+
+	@Path("/json")
+	@GET
+	public String getAccount(String account) {
+		return service.getAccount(account);
+	}
+
+	@Path("/json")
+	@POST
+	public String createAccount(String account) {
+		return service.createAccount(account);
+	}
+
+	@Path("/json")
+	@DELETE
+	public String deleteAccount(String account) {
+		return service.deleteAccount(account);
+	}
+
+	@Path("/json")
+	@PUT
+	public String updateAccount(String account) {
+		return service.updateAccount(account);
+	}
+
+	public void setService(MealService service) {
+		this.service = service;
+	}
 }
